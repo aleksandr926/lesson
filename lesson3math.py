7c823cab6adb186e3cf3857f4ef1ecd6add5047f
aleksandr926 <72929711+aleksandr926@users.noreply.github.com>
2020-10-15 18:45:16 +0400

    first commit

---
+++ lesson3math.py
@@ -0,0 +1,116 @@
+# x = 45
+# y = 10 
+# result = x%y
+# print(result)
+
+# x = 10
+# y = 3
+# result = x//y
+# print(result)
+
+# x = 3
+# y = 10
+# result = x//y
+# print(result)
+
+# x = 4
+# y = 3 
+# result = x**y
+# print(result)
+
+# x = 16
+# y = 0.5
+# result = x ** y
+# print(result)
+
+# balance = 0
+# paycheck = 150
+# new_balance = balance + paycheck
+# print(new_balance)
+
+# balance = 100
+# balance = balance + 150
+
+# balance = 100
+# balance += 150
+# print(balance)
+
+# balance = 100
+# balance -= 150
+# print(balance)
+
+# balance = 0
+# print("\n\t\tHello")
+# deposit = input("\n\tDo you want to deposit cash? (Yes or No) ")
+# print(deposit)
+# new_cash = int(input("\n\t How much money do you want to deposit? "))
+# balance += new_cash
+# print("Your current cash",balance,'$')
+
+# x = 5
+# y = 7 
+# result = x>y
+# print(result)
+
+# x = 5 
+# y = 7 
+# result = x<y
+# print(result)
+
+# x = 5 
+# y = 7 
+# result = x>=y
+# print(result)
+
+# x = 5 
+# y = 7 
+# result = x<=y
+# print(result)
+
+# x = 5
+# y = 7 
+# result = x==y
+# print(result)
+
+# x = 5
+# y = 7
+# result =x != y
+# print(result)
+
+# balance = 1000
+# withdrowal = 10000
+# print(balance>=withdrowal)
+
+# current_class = int(input("Please mention the number of students "))
+# number = 16
+# result = current_class > number 
+# print(result)
+# odd_or_even = current_class%2
+# print(odd_or_even==1)
+# current_class-=1
+# class1 = current_class/2
+# class2 = class1 + 1
+# print(class1,class2)
+	
+# current_class = int(input("Please mention the number of students "))
+# number = 16
+# result = current_class > number 
+# print(result)
+# odd_or_even = current_class%2
+# print(odd_or_even==0)
+# number_of_students = current_class/2
+# print(number_of_students)
+
+# our_wakeup_time = 6
+# battary = int(input("When do you want me to turn off battary? "))
+# waketime = int(input("When do you usally wake up? "))
+# waketime2 = int(input("When do you usally wake up? "))
+# waketime3 = int(input("When do you usally wake up? "))
+# mean = (waketime + waketime2 + waketime3)/3
+# print(battary,mean)
+# print(our_wakeup_time==mean)
+# print("Succsesful operation")
+
+
+
+
